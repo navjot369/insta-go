156b6c9c20f1b9551ebb129c7990b26043ae8371
MayankBansal <maynakbansal51351@gmail.com>
2024-12-29 12:17:46 +0530

    some minor theme changes

--- frontend/src/components/Landing/index.jsx
+++ frontend/src/components/Landing/index.jsx
@@ -1,5 +1,3 @@
-
-
 import Hero from "./../Hero/Hero";
 import About from "./../About/About";
 import Services from "./../Services/Services";
@@ -7,8 +5,8 @@ import Reviews from "./../Reviews/Reviews";
 
 export default function Landing({theme}) {
     return (<>
-        <Hero theme={theme}/>
-      <About theme={theme}/>
-      <Services theme={theme}/>
-      <Reviews theme={theme}/></>);
+      <Hero/>
+      <About/>
+      <Services/>
+      <Reviews/></>);
 };
